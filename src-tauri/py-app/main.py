c6c2c56bdc24c7de9b0fc38158ccd62ca7f0342c
srnot <yikmanau@gmail.com>
2024-03-15 03:17:02 +0800

    Added barebone template

--- src-tauri/py-app/main.py
+++ src-tauri/py-app/main.py
@@ -1,5 +1,5 @@
 
 
-def main():
+def main(dummy):
     return "Hello, Python"
 
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,12 +3,16 @@
 use std::fmt::format;
 use std::fs;
 use std::path::Path;
+use std::str::FromStr;
 
 use pyo3::prelude::*;
 use pyo3::types::*;
 use tauri::Manager;
 
+mod pyhelpers;
+
 const PY_VENV_PATH: &str = r#"D:\RustRoot\RustPolars\00_venv\portopt\Lib\site-packages"#;
+
 
 fn main() {
     let app = tauri::Builder::default().setup(|app| {
@@ -20,16 +24,29 @@ fn main() {
         Ok(())
     });
 
-    app.invoke_handler(tauri::generate_handler![tauri_main,])
+    app.invoke_handler(tauri::generate_handler![serve_string_python,])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 #[tauri::command(rename_all = "snake_case")]
-fn tauri_main() -> String {
+fn serve_string_python() -> String {
+    let func_args = (&String::from("None"), );
 
-  let pysrc = pysrc_main();
-  let py_res = pyfunc_runtime(PY_VENV_PATH, pysrc, "main", ("None", ));
+    let pysrc = pyhelpers::pysrc_main();
+    let py_res = pyhelpers::pyfunc_runtime(PY_VENV_PATH, pysrc, "main", func_args);
 
-  format!("Hello, Rust and {}", py_res)
+    format!("Hello, Rust and {}", py_res)
+}
+
+mod test {
+    use crate::serve_string_python;
+
+    #[test]
+    fn test_py_str() {
+        assert_eq!(
+            serve_string_python(),
+            "Hello, Rust and Hello, Python".to_string()
+        )
+    }
 }
